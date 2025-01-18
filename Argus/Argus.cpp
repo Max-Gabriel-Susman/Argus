ca01f1f4497455e92f664255cb62db5d73af369f
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-01-18 13:37:08 -0700

    first commit

---
+++ Argus/Argus.cpp
@@ -0,0 +1,16 @@
+﻿// Argus.cpp : Defines the entry point for the application.
+//
+
+#include "Argus.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Initializing Argus..." << endl;
+
+	// Initialize Argus
+
+	cout << "Argus online." << endl;
+	return 0;
+}
