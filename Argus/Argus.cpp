8b7bf05d8051914474cc46e0236746f83c6f1791
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-01-18 17:17:52 -0700

    got a rudimentary gstreamer pipeline working in the vs 22 cmake template

--- Argus/Argus.cpp
+++ Argus/Argus.cpp
@@ -1,16 +1,33 @@
-﻿// Argus.cpp : Defines the entry point for the application.
-//
+﻿#include "Argus.h"
+#include <gst/gst.h>  // GStreamer main header
 
-#include "Argus.h"
-
-using namespace std;
-
-int main()
+int main(int argc, char* argv[])
 {
-	cout << "Initializing Argus..." << endl;
+    // Standard GStreamer init
+    gst_init(&argc, &argv);
 
-	// Initialize Argus
+    g_print("Initializing Argus with GStreamer...\n");
 
-	cout << "Argus online." << endl;
-	return 0;
+    // Create a simple test pipeline: videotestsrc ! autovideosink
+    GError* error = nullptr;
+    GstElement* pipeline = gst_parse_launch("videotestsrc ! autovideosink", &error);
+
+    if (!pipeline) {
+        g_printerr("Pipeline parse error: %s\n", error->message);
+        return -1;
+    }
+
+    // Start playing
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    // Let it run a little
+    g_print("Pipeline is playing...\n");
+    g_usleep(3 * G_USEC_PER_SEC);  // sleep 3 seconds
+
+    // Shutdown
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+
+    g_print("Argus shutting down.\n");
+    return 0;
 }
